1f573e8342fba132d0a1230b99dd94b4b4a0913c
Weinan Li <l.weinan@gmail.com>
2018-04-20 21:20:04 +0800

    .

---
+++ src/main/java/io/weli/PlayWithJBossLogger.java
@@ -0,0 +1,7 @@
+package io.weli;
+
+public class PlayWithJBossLogger {
+    public static void main(String[] args) {
+        System.out.println(java.util.logging.Logger.getLogger("test"));
+    }
+}
