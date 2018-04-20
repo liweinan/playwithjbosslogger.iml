0cb2f35b293a1f42b356e1bb442d82523d1ef05b
Weinan Li <l.weinan@gmail.com>
2018-04-20 22:22:03 +0800

    .

--- src/main/java/io/weli/PlayWithJBossLogger.java
+++ src/main/java/io/weli/PlayWithJBossLogger.java
@@ -1,7 +1,15 @@
 package io.weli;
 
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Logger;
+
 public class PlayWithJBossLogger {
     public static void main(String[] args) {
+        System.setProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager");
         System.out.println(java.util.logging.Logger.getLogger("test"));
+        Logger logger = java.util.logging.Logger.getLogger("test");
+        logger.addHandler(new ConsoleHandler());
+        logger.warning("前方高能！");
     }
 }
