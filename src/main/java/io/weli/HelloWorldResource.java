cc2e6d2a157cc89cb4a659c2e29ec943ae241475
Weinan Li <l.weinan@gmail.com>
2018-04-23 01:33:25 +0800

    .

--- src/main/java/io/weli/HelloWorldResource.java
+++ src/main/java/io/weli/HelloWorldResource.java
@@ -1,12 +1,29 @@
 package io.weli;
 
+import org.jboss.resteasy.spi.ResteasyConfiguration;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/hello")
 public class HelloWorldResource {
     @GET
     public String sayHello() {
         return "Hello, world!";
+    }
+
+    @GET
+    @Path("/uri")
+    public String uri(@Context UriInfo uriInfo) {
+        return uriInfo.getPath().toString();
+
+    }
+
+    @GET
+    @Path("/config")
+    public String config(@Context ResteasyConfiguration config) {
+        return config.getInitParameter("foo");
     }
 }
