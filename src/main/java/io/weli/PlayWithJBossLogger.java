ece5efb18595f29333862272aeba00f914326416
Weinan Li <l.weinan@gmail.com>
2018-04-25 23:58:40 +0800

    .

--- src/main/java/io/weli/PlayWithJBossLogger.java
+++ src/main/java/io/weli/PlayWithJBossLogger.java
@@ -3,17 +3,19 @@ package io.weli;
 
 import org.jboss.logmanager.LogManager;
 
-import java.util.logging.Logger;
 
 public class PlayWithJBossLogger {
     public static void main(String[] args) throws Exception {
         System.setProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager");
 //        System.setProperty("java.util.logging.config.file", PlayWithJBossLogger.class.getResource("/").getPath() + "logging.jboss.properties");
         LogManager.getLogManager().readConfiguration(UndertowWithLogger.class.getClassLoader().getResourceAsStream("logging.jboss.properties"));
-        Logger logger = Logger.getLogger("test");
+        org.jboss.logging.Logger logger = org.jboss.logging.Logger.getLogger(PlayWithJBossLogger.class.toString());
+
+
         System.out.println(logger);
-        logger.warning("前方高能！");
+        logger.warn("前方高能！");
         logger.info("普通消息");
-        logger.fine("调试消息");
+        logger.debug("调试消息");
+        logger.trace("跟踪信息");
     }
 }
